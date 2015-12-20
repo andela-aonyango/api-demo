242c5957bce6b409ea53d3c4618b577bd79dcc97
andela-aonyango <andela-aonyango@users.noreply.github.com>
2015-12-20 12:04:32 +0300

    set up a route to create a user

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,23 @@ var express = require('express'),
 var mongoose = require('mongoose'),
   User       = require('../models/user.js');
 
-/* GET users listing. */
+// get users
 router.get('/', function(request, response, next) {
   User.find(function(error, users){
     if (error) {
       return next(error);
     }
     response.json(users);
+  });
+});
+
+// create a user
+router.post('/', function(request, response, next) {
+  User.create(request.body, function(error, post) {
+    if (error) {
+      return next(error);
+    }
+    response.json(post);
   });
 });
 
