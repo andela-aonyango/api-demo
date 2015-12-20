5fd45849386d420b0e6e634f87a18c0deffd5623
andela-aonyango <andela-aonyango@users.noreply.github.com>
2015-12-20 09:44:08 +0300

    set up a route to retrieve users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+  router    = express.Router();
+
+var mongoose = require('mongoose'),
+  User       = require('../models/user.js');
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function(request, response, next) {
+  User.find(function(error, users){
+    if (error) {
+      return next(error);
+    }
+    response.json(users);
+  });
 });
 
 module.exports = router;
